7837cdccd913c60f089433f4d136e45ff13a1bfb
Jake <jdai54@gmail.com>
2017-04-02 15:17:27 -0700

    Assignment 5 Angular: Stuck

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,6 +1,6 @@
 (function() {
     function CollectionCtrl() {
-      // instead of using jQuery to append images, bind th edata from albumPicasso object to the Collection template //
+      // instead of using jQuery to append images, bind the data from albumPicasso object to the Collection template //
       this.albums = [];
       for (var i=0; i < 12; i++) {
         this.albums.push(angular.copy(albumPicasso));
