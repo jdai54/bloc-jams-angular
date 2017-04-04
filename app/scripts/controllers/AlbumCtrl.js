bf1abe605fdf6c4873c6c9401e59ac0c0e586632
Jake <jdai54@gmail.com>
2017-04-03 18:50:56 -0700

    Checkpoint 6 Angular: Complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,12 @@
 (function() {
-  function AlbumCtrl() {
-    this.albumData = angular.copy(albumPicasso);
+  // inject the custom Fixtures service into the controller by passing it as the paramenter and as a dependency //
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
   }
   
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+    // add Fixtures to AlbumCtrl's array of dependencies. Once injected the service is available for use within the controller //
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
 
