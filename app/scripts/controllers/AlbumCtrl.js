eecd7a97270b27ba8a8398fee7dea2716f6ba33e
Jake <jdai54@gmail.com>
2017-04-05 11:00:27 -0700

    Checkpoint 7 Angular: Question

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
   // inject the custom Fixtures service into the controller by passing it as the paramenter and as a dependency //
-  function AlbumCtrl(Fixtures) {
+  function AlbumCtrl(Fixtures, SongPlayer) {
     this.albumData = Fixtures.getAlbum();
+    this.songPlayer = SongPlayer;
   }
   
   angular
     .module('blocJams')
-    // add Fixtures to AlbumCtrl's array of dependencies. Once injected the service is available for use within the controller //
-    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    // add Fixtures to AlbumCtrl's array of dependencies. Once injected the service is available for use within the controller. Also since we will be playing music from the album view, the SongPlayer service is injected into AlbumCtrl //
+    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
 
