fb9c5f962cac3ab01008bebf3738c49689d23398
Jake <jdai54@gmail.com>
2017-07-05 22:34:50 -0700

    Installed Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  grunt.registerTask('build', ['clean', 'copy']);
+
+  grunt.initConfig({
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          src: ['./assets/**/*.{png,jpg,jpeg,mp3}'],
+          dest: './dist',
+          cwd: './app'
+        }, {
+          expand: true,
+          src: ['./**/*.html'],
+          dest: './dist',
+          cwd: './app/pages'
+        }, {
+          expand: true,
+          src: ['./**/*.css'],
+          dest: './dist/styles',
+          cwd: './app/styles'
+        }, {
+          expand: true,
+          src: ['./**/*.js'],
+          dest: './dist/scripts',
+          cwd: './app/scripts'
+        }, {
+          expand: true,
+          src: ['./**/*.html'],
+          dest: './dist/templates',
+          cwd: './app/templates'
+        }]
+      }
+    },
+
+    clean: ['./dist']
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+};
